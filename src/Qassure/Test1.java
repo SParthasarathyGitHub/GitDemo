74e3d1859cc0188835b2303a74a2107c7094a179
shri <shrisarathy23@gmail.com>
2024-02-28 21:25:32 +0800

    Add Payee to main

--- src/Qassure/Test1.java
+++ src/Qassure/Test1.java
@@ -3,7 +3,7 @@ package Qassure;
 public class Test1 {
 	public static void main(String[] args) {
 
-		System.out.println("hi-Brach2");
+		System.out.println("hi-main");
 
 		System.out.println("Add payee");
 		System.out.println("Error");
